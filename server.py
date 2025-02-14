ee943d0c3649cdd125eff4439d662b00ca3a8be5
Yining Hong <yhong3@andrew.cmu.edu>
2025-02-14 13:18:28 -0500

    finish lab5

--- server.py
+++ server.py
@@ -4,27 +4,30 @@ import joblib
 
 app = Flask(__name__)
 
-# TODO: Load the trained model from the shared volume (use the correct path)
-model = ...
+# DONE: Load the trained model from the shared volume (use the correct path)
+model = joblib.load("/app/models/iris_model.pkl")
 
-# TODO: Add request method 
-@app.route('/predict', methods=[''])
+
+# DONE: Add request method
+@app.route("/predict", methods=["GET"])
 def predict():
-    # TODO: Get the input array from the request body
+    # DONE: Get the input array from the request body
     get_json = request.get_json()
-    iris_input = get_json['input']
+    iris_input = get_json["input"]
 
-    # TODO: Make prediction using the model 
+    # DONE: Make prediction using the model
     # HINT: use np.array().reshape(1, -1) to convert input to 2D array
-    prediction = ...
+    prediction = model.predict(np.array(iris_input).reshape(1, -1))
 
-    # TODO: Return the prediction as a response
-    return ...
+    # DONE: Return the prediction as a response
+    return jsonify({"prediction": prediction.tolist()})
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return 'Welcome to Docker Lab'
+    return "Welcome to Docker Lab"
 
-if __name__ == '__main__':
-    # TODO: Run the Flask app (bind it to port 8080 or anyt other port)
-    app.run(debug=True, port=8080, host='0.0.0.0')
+
+if __name__ == "__main__":
+    # DONE: Run the Flask app (bind it to port 8080 or anyt other port)
+    app.run(debug=True, port=8080, host="0.0.0.0")
